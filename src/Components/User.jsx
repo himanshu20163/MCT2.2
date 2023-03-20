b35f1cde86dd3dc309600572ac2357108bb9e6d4
himanshu20163 <109578416+himanshu20163@users.noreply.github.com>
2023-03-20 06:24:56 -0700

    Add files via upload

---
+++ src/Components/User.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import './user.css';
+
+const User = () => {
+  const [genderResult, setGenderResult] = useState({ results: [] });
+  const [selectedGender, setSelectedGender] = useState("all");
+  
+
+  const fetchData = async () => {
+    const url = selectedGender === 'all'
+      ? "https://randomuser.me/api/?results=20"
+      : `https://randomuser.me/api/?results=20&gender=${selectedGender}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    setGenderResult(data);
+    console.log(data);
+  }
+
+  const handleGenderChange = (event) => {
+    const genderValue = event.target.value;
+    setSelectedGender(genderValue);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [selectedGender]);
+
+  return (
+    <>
+      <div className='maincontaineruser'>
+        <div className='subcontaineruser'>
+          <h1>Users</h1>
+          <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+        </div>
+        <div className='inputsradi'>
+        <label><input type="radio" name='gender' value="all" onChange={handleGenderChange} checked={selectedGender === 'all'} />All</label>
+         <label><input type="radio" name='gender' value="male" onChange={handleGenderChange} checked={selectedGender === 'male'} />Male</label>
+          <label><input type="radio" name='gender' value="female" onChange={handleGenderChange} checked={selectedGender === 'female'} />Female</label>
+        </div>
+        <div className='subnavuser'>
+          <span className='imagewala'>IMAGE</span>
+          <span className='namewala'>NAME</span>
+          <span className='emailwala'>EMAIL</span>
+          <span className='genderwala'>GENDER</span>
+        </div>
+        {genderResult.results.map((ele) => (
+          <div className='cards' key={ele.login.uuid}>
+            <img src={ele.picture.large} alt="pic" ></img>
+            <h3>{ele.name.first}&nbsp;&nbsp;{ele.name.last}</h3>
+            <h4>{ele.email}</h4>
+            <h2>{ele.gender}</h2>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default User;
